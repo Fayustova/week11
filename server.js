5c16de0c28a85e9d7a8593cc491d5f36669726cc
Fayustova <jelizaveta.fayustova@tktk.ee>
2024-11-18 15:52:26 +0200

    week 10

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const db = require('./db');
 const recipeRouter = require('./routes/recipes.routes');
 const ingredientRouter = require('./routes/ingredients.routes');
+const fullRecipesRouter = require('./routes/fullRecipes.routes');
 const app = express();
 
   
 
 app.use('/recipes', recipeRouter);
 app.use('/ingredients', ingredientRouter);
+app.use('/fullRecipes', fullRecipesRouter);
+
 
 app.listen(3000, () => {
     console.log('Server is running on Port 3000.');
